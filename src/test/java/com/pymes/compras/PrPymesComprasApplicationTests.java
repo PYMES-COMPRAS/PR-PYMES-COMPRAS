6461e7450570bde1f96a187390e6f87634ca3d2f
EdwardJimenez <jve0021477@est.univalle.edu>
2022-09-22 11:24:08 -0400

    Creacion de entorno de desarollo SpringBoot

---
+++ src/test/java/com/pymes/compras/PrPymesComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pymes.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrPymesComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
