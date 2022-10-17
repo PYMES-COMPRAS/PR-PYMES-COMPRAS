bfaa67b30e866116c8f691a4e79c736a3e22940b
Andy Paul Guzman Escalera <andyguzman117@gmail.com>
2022-10-17 11:36:06 -0400

    first commit

---
+++ microservicios-libro-diario/src/main/java/com/prpymes/microservicios/app/librodiario/MicroserviciosLibroDiarioApplication.java
@@ -0,0 +1,13 @@
+package com.prpymes.microservicios.app.librodiario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosLibroDiarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosLibroDiarioApplication.class, args);
+	}
+
+}
