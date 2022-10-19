55ad8cd5f59e5099bd50a0e258d1faa839800231
Andy Paul Guzman Escalera <andyguzman117@gmail.com>
2022-10-18 21:59:01 -0400

    updates base

--- microservicios-libro-diario/src/main/java/com/prpymes/microservicios/app/librodiario/MicroserviciosLibroDiarioApplication.java
+++ microservicios-libro-diario/src/main/java/com/prpymes/microservicios/app/librodiario/MicroserviciosLibroDiarioApplication.java
@@ -2,7 +2,8 @@ package com.prpymes.microservicios.app.librodiario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviciosLibroDiarioApplication {
 
