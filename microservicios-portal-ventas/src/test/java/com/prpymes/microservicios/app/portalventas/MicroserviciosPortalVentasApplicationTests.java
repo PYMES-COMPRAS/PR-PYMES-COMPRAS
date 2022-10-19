305b4afee7aa6d72497fc2140ad6fb80a988188e
Andy Paul Guzman Escalera <andyguzman117@gmail.com>
2022-10-18 23:31:16 -0400

    first commit usuario

---
+++ microservicios-portal-ventas/src/test/java/com/prpymes/microservicios/app/portalventas/MicroserviciosPortalVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prpymes.microservicios.app.portalventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosPortalVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
