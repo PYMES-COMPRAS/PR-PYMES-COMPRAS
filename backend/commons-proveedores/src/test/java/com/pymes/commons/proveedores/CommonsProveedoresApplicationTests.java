0e197ca91c02b395326b5812bba23d9364d3d083
EdwardJimenez <jve0021477@est.univalle.edu>
2022-10-27 11:05:40 -0400

    Armado de proyecto base y parte de listado de proveedores

---
+++ backend/commons-proveedores/src/test/java/com/pymes/commons/proveedores/CommonsProveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pymes.commons.proveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsProveedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
