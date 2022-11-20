d042a8f9abe70b8e1c76503825921181377fe01f
EdwardJimenez <jve0021477@est.univalle.edu>
2022-11-20 16:57:49 -0400

    modificaciones en las clases entity de relaciones onetomany a manytoone

--- backend/commons-models/src/main/java/com/pymes/commons/models/entity/Manufactura.java
+++ backend/commons-models/src/main/java/com/pymes/commons/models/entity/Manufactura.java
@@ -1,14 +1,18 @@
 package com.pymes.commons.models.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@Entity
+@Table(name = "pm_c_manufactura")
 public class Manufactura {
     
     @Id
