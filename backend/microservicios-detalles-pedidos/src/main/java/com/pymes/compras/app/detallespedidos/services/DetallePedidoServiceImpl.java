fb16a000299edc413e92d88b7d872270fa5ea500
EdwardJimenez <jve0021477@est.univalle.edu>
2022-11-21 02:19:28 -0400

    metodo de consulta para de fechas para libro diario

--- backend/microservicios-detalles-pedidos/src/main/java/com/pymes/compras/app/detallespedidos/services/DetallePedidoServiceImpl.java
+++ backend/microservicios-detalles-pedidos/src/main/java/com/pymes/compras/app/detallespedidos/services/DetallePedidoServiceImpl.java
@@ -1,6 +1,12 @@
 package com.pymes.compras.app.detallespedidos.services;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pymes.commons.models.entity.DetallePedido;
 import com.pymes.commons.services.CommonServiceImpl;
@@ -8,5 +14,12 @@ import com.pymes.compras.app.detallespedidos.models.repository.DetallePedidoRepo
 
 @Service
 public class DetallePedidoServiceImpl extends CommonServiceImpl<DetallePedido, DetallePedidoRepository> implements DetallePedidoService {
+
+    @Override
+    public List<Object> findByDateTotal(@Param("from") LocalDate from,@Param("to") LocalDate to) {
+        return repository.findByDateTotal(from, to);
+    }
+
     
+
 }
