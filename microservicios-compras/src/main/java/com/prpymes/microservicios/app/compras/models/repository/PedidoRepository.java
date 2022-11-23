4cc3e5a750e88db126666ba37c082d5a1e3aa214
EdwardJimenez <jve0021477@est.univalle.edu>
2022-11-23 17:34:21 -0400

    Corrigiendo consulta de PedidoRepository de mostrar productos de pedidos

--- microservicios-compras/src/main/java/com/prpymes/microservicios/app/compras/models/repository/PedidoRepository.java
+++ microservicios-compras/src/main/java/com/prpymes/microservicios/app/compras/models/repository/PedidoRepository.java
@@ -10,7 +10,7 @@ import com.prpymes.microservicios.app.compras.models.entity.Pedido;
 
 public interface PedidoRepository extends PagingAndSortingRepository<Pedido, Integer> {
     
-    @Query(value = "SELECT po.nombre as Nombre, dp.cantidad as Producto, dp.descuento as Descuento, dp.precio_unitario as 'Precio Unitario' FROM pm_c_pedido p INNER JOIN pm_c_detallepedido dp ON p.id_pedido = dp.id_pedido INNER JOIN pm_b_producto po ON po.id_producto = dp.id_producto WHERE p.id_pedido =:id", nativeQuery = true)
+    @Query(value = "SELECT po.nombre as Nombre, dp.cantidad as Producto, dp.descuento as Descuento, dp.precio_unitario as 'Precio Unitario' FROM pm_c_pedido p INNER JOIN pm_c_detallepedido dp ON p.id = dp.id_pedido INNER JOIN pm_b_producto po ON po.id = dp.id_producto WHERE p.id =:id", nativeQuery = true)
     public List<Object> showPedidoById(@Param("id") Integer id);
     
 }
